5a476268550cc2c4ca889ba1a51e37147d02eb69
Edward Huang <huangedward00@gmail.com>
2026-05-28 21:21:06 -0400

    Add: insert-delete-getrandom-o1 - submission-1

---
+++ Data Structures & Algorithms/insert-delete-getrandom-o1/submission-1.py
@@ -0,0 +1,40 @@
+class RandomizedSet:
+
+    def __init__(self):
+        ## set() O(1) insert remove, no O(1) random, since have to get all elements
+        ## {} key = val, value = index where value stored in array, []
+        self.hashMap = {}
+        self.values = []
+
+    def insert(self, val: int) -> bool:
+        if val in self.hashMap:
+            return False
+        
+        self.hashMap[val] = len(self.values)
+        self.values.append(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.hashMap:
+            return False
+        
+        lastIndex = len(self.values) - 1
+        index = self.hashMap[val]
+
+        self.hashMap[self.values[lastIndex]] = index
+        self.values[index] = self.values[lastIndex]
+        self.values.pop()
+        del self.hashMap[val]
+
+        return True
+
+
+    def getRandom(self) -> int:
+        return random.choice(self.values)
+        
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
